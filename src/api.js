54293e984b7be270380a6cd42f0ebe1d1d53b0ff
flibustier <contact@jonathan.pl>
2018-01-08 17:55:53 +0100

    Add static + prod api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseURLprod = "http://dev.skillmatrix.be/app_dev.php/api/v1"
-const baseURL = "http://127.0.0.1:8000/app_dev.php/api/v1"
+const baseURL = "http://dev.skillmatrix.be/app_dev.php/api/v1"
 
 // Get requests list
 const requests = () =>
